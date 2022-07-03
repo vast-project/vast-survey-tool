630e6ee16534fd071dbe0ee50d702e7cb347ce9a
petasis <petasisg@yahoo.gr>
2022-07-03 15:33:38 +0300

    VAST Logo as png in all platforms

--- VAST Survey Tool/VAST Survey Tool/ViewModels/AboutViewModel.cs
+++ VAST Survey Tool/VAST Survey Tool/ViewModels/AboutViewModel.cs
@@ -7,12 +7,19 @@ namespace VAST_Survey_Tool.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private const string File = "vast_logo.png";
+
         public AboutViewModel()
         {
             Title = AppResources.About;
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://www.vast-project.eu/"));
+            /*VASTLogo = new Image { };
+            VASTLogo.Source = Device.RuntimePlatform == Device.Android
+                ? ImageSource.FromFile("vast_logo.xml")
+                : ImageSource.FromFile(File);*/
         }
 
         public ICommand OpenWebCommand { get; }
+        // public Image VASTLogo { get; }
     }
 }
